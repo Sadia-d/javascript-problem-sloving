63803e26278a55a4215c1fd37adc8e355b41f455
Sadia-d <subai>
2025-10-19 11:37:00 +0600

    array.js

--- array.js
+++ array.js
@@ -1,3 +1,100 @@
+
+// Create an array with 5 fruits and print each fruit using a loop.
+
+let fruits = ['apple','jackfruit',' orange','mango','berry'];
+
+for(let fruit of fruits){
+//    console.log(fruit)
+ };
+
+//Find the length of an array.
+
+let array = [1,2,3,4,5];
+// console.log(array.length);
+
+
+// Add one new element to the end of an array.
+let addNewEnd = [1,2,3,4]
+addNewEnd.push(5);
+// console.log(addNewEnd);
+
+//Add one new element to the beginning of an array
+
+let addNewFirst = [1,2,3,4]
+addNewFirst.unshift(0);
+// console.log(addNewFirst);
+
+
+// Check if a given item exists in an array or not.
+
+let exitElement = [1,2,34,52,5];
+
+if(exitElement.includes(5)){
+        // console.log('true')
+    }
+    else{
+        // console.log("false");       
+    
+}
+  
+/// Convert an array into a single string separated by commas.
+
+let convert = ['Hello','I','am','sadia'];
+let join = convert.join()
+
+// console.log(join)
+
+
+/// Reverse an array without using a loop.
+
+let reverseArrays = [1,2,3,4,5,6];
+reverseArrays.reverse();
+// console.log(reverseArrays)
+
+
+// Create a new array that contains only positive numbers.
+
+let positiveNumbers =[1,2,-3,5,3,-5,4,-1];
+for(let number of positiveNumbers){
+    if(number >0){
+        // console.log(number);        
+    }
+}
+
+
+// Use .map() to square each number in an array.
+
+let squareArray =[1,2,3,4,5,6];
+let square=squareArray.map((x)=> x*x);
+// console.log(square)
+
+
+// Use .filter() to get numbers greater than 10.
+
+let filterMaxNum = [12,3,4,232,523,2,31,1]
+// console.log(filterMaxNum.filter((a)=> a >10));
+
+//Use .reduce() to calculate the total sum of all numbers.
+
+let sumOfNumbers = [1,2,3,4,5,6]
+// console.log(sumOfNumbers.reduce((acc,curr) => acc + curr));
+
+/// Use .sort() to sort numbers in ascending order.
+  
+let sortNumber = [12,32,43,2,11]
+// console.log(sortNumber.sort())
+//.sort() method arr-এর elements গুলোকে ascending (alphabetical) order-এ সাজায় এবং মূল array-টিকেই পরিবর্তন করে।
+
+/// Merge two arrays into one.
+let array1 = [1,2,3,4]
+let array2 =[5,6,7]
+// console.log(array1.concat(array2));
+
+// Check if all numbers are positive using .every().
+
+let checkPositive = [1,2,3,-4,5,6]
+console.log(checkPositive.every((n) => n >0)) // It returns false if it finds one element in the array that does not exist Otherwise, it returns true.
+
 // Write a program to find the largest number in an array.
 
 function maxNumberOfArray(numbers){
